6fc06ae405d01bc1cf70bd7009f9827022fa1474
Arist0craft <dimnktn@gmail.com>
2021-11-08 03:20:11 +0300

    Added preparations for basic deploy Add: uvicorn and django-environ packages Change: way to get variables in settings.py

--- app/settings.py
+++ app/settings.py
@@ -11,21 +11,33 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from typing import List
+
+
+import environ
+
+env = environ.Env(
+    DEBUG=(bool, True),
+    ALLOWED_HOSTS=(list, [])
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+environ.Env.read_env(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=(ut1(8&an4zy!(y1^csh9!^0_0ww0_usguri7b8h=hj8jbrqm'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = env("ALLOWED_HOSTS")
 
 
 # Application definition
@@ -75,14 +87,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'polls',
-        'USER': 'dev',
-        'PASSWORD': '12345678',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
-    }
+    'default':  env.db()
 }
 
 
